5fa07e84510689770397674ea093a20b6f9a01d4
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-10-15 21:03:48 +0530

    [Umesh]Added: welcome message

---
+++ src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollService.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.employeepayroll;
+
+public class EmployeePayrollService {
+	
+	public static void main(String[] args) {
+		System.out.println("welcome to employee payroll service problem");
+	}
+}
