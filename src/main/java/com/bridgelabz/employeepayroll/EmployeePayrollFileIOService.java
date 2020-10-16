8d765e09505e64fc5da27c35d170a99493100eb0
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-10-16 13:39:08 +0530

    [Umesh]Added: watch service to watch a particular directory

---
+++ src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollFileIOService.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.employeepayroll;
+
+import java.util.List;
+
+public class EmployeePayrollFileIOService {
+	
+}
--- src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollService.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Scanner;
 
 public class EmployeePayrollService {
+	public enum IOService {CONSOLE_IO, FILE_IO, DB_IO, REST_IO}
+	
 	private List<EmployeePayrollData> employeePayrollList;
 
 	public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
@@ -14,18 +16,18 @@ public class EmployeePayrollService {
 	public static void main(String[] args) {
 		ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
 		EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
-		Scanner sc = new Scanner(System.in);
-		employeePayrollService.readEmployeePayrollData(sc);
+		Scanner consoleInputReader = new Scanner(System.in);
+		employeePayrollService.readEmployeePayrollData(consoleInputReader);
 		employeePayrollService.writeEmployeePayrollData();
 	}
 
-	public void readEmployeePayrollData(Scanner sc) {
+	public void readEmployeePayrollData(Scanner consoleInputReader) {
 		System.out.println("Enter Employee id: ");
-		int id = sc.nextInt();
+		int id = consoleInputReader.nextInt();
 		System.out.println("Enter Employee Name: ");
-		String name = sc.next();
+		String name = consoleInputReader.next();
 		System.out.println("Enter Employee salary: ");
-		double salary = sc.nextDouble();
+		double salary = consoleInputReader.nextDouble();
 		employeePayrollList.add(new EmployeePayrollData(id, name, salary));
 	}
 
